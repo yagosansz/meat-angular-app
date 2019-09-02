2d5c069521ddca30d89f15fcc30e7dd48a687109
Yago Santos de Sousa <yagosansz@gmail.com>
2019-09-02 12:17:41 -0400

    Added animation w/ Keyframes to ShoppingCartComponent

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 import { CartItem } from './cart-item.model';
 import { MenuItem } from './../menu-item/menu-item.model';
@@ -7,10 +8,27 @@ import { ShoppingCartService } from './shopping-cart.service';
 
 @Component({
   selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      state('ready', style({ opacity: 1 })), // Makes sure that the element will be visible in the ready state!
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 1 })
+      ]))),
+      transition('ready => void', animate('300ms 0s ease-out', keyframes([
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+        style({ opacity: 0, transform: 'translateX(30px)', offset: 1 })
+      ])))
+    ])
+  ]
 })
 
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready'
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
